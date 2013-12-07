52076a787979b60118fcf6d00265788945674279
Akihiro Maeda <sola.1980.a@gmail.com>
2013-12-07 15:04:01 +0900

    Updated the JCROM to android 4.4.1_r1.

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1370,8 +1370,6 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
         for (String numeric : numericArray) {
             if (operatorNumeric.startsWith(numeric))
                 return true;
-            else
-                return false;
         }
         return false;
     }
